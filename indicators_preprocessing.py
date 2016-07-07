8e3d00c68f8d220165b595be1f60f6db32e01746
curtcjay <cjames@stern.nyu.edu>
2016-07-07 12:19:42 -0400

    PEP8

--- indicators_preprocessing.py
+++ indicators_preprocessing.py
@@ -1,7 +1,8 @@
 import pandas as pd
 import os
 
-filedir = os.path.dirname(os.path.realpath(__file__)) #get this file directory
+filedir = os.path.dirname(os.path.realpath(__file__))  # get this file directory
+
 
 def indicators_preprocessing():
     """
@@ -9,12 +10,12 @@ def indicators_preprocessing():
     Return: a merged dataset containing 1980-2015 indicators data.
     """
     s_year = range(1980, 2016)
-    #load indicators data into a dictionary
-    dfs = {year:pd.read_csv(filedir+"/data/stats_{}.csv".format(year),encoding = "ISO-8859-1") for year in s_year}
-    
+    # load indicators data into a dictionary
+    dfs = {year: pd.read_csv(filedir + "/data/stats_{}.csv".format(year), encoding="ISO-8859-1") for year in s_year}
+
     for year in s_year:
         dfs[year] = dfs[year].set_index(['Country'])
-    return pd.concat(dfs, axis=1, join='outer') #merge dataframes into a dataframe
+    return pd.concat(dfs, axis=1, join='outer')  # merge dataframes into a dataframe
 
 
 def indicators_preprocessing_by_year(indc='indc'):
@@ -22,11 +23,12 @@ def indicators_preprocessing_by_year(indc='indc'):
     This function is to prepocess indicator data by year
     Return:
     indicators_by_year: a dataframe containing every year's indicators for each country from 1980 to 2015
+    :rtype: object
     """
     df = indicators_preprocessing()
     df.columns = df.columns.get_level_values(1)
     indicators_by_year = df[indc]
-    indicators_by_year.columns= range(1980,2016) #rename columns names with years
+    indicators_by_year.columns = range(1980, 2016)  # rename columns names with years
     return indicators_by_year
 
 
@@ -34,15 +36,16 @@ def indicators_preprocessing_for_table(indc='indc'):
     """
     This function is to prepocess indicator data by year
     Return:
-    indicators_for_table: a dataframe containing every year's indicators for each country from 1980 to 2015 for the table tab
+    indicators_for_table: a dataframe containing every years indicators for each country from 1980 to 2015
+    for the table tab
     """
     df = indicators_preprocessing()
     df.columns = df.columns.get_level_values(1)
     indicators_for_table = df[indc]
-    indicators_for_table.columns= range(1980,2016)
+    indicators_for_table.columns = range(1980, 2016)
     indicators_for_table['Country'] = df.index
     cols = indicators_for_table.columns.tolist()
     cols = cols[-1:] + cols[:-1]
     indicators_for_table = indicators_for_table[cols]
-    indicators_for_table = indicators_for_table.reset_index(drop = True)
+    indicators_for_table = indicators_for_table.reset_index(drop=True)
     return indicators_for_table
